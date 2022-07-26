3cf4b2eb21e0823d76c36bcd4b3ef97f4183ab1a
Marta Arenas <martilla311@gmail.com>
2022-07-26 09:43:52 +0200

    Barra de navegación

--- app6_heroesapp/src/HeroesApp.jsx
+++ app6_heroesapp/src/HeroesApp.jsx
@@ -1,9 +1,11 @@
+import { Navbar } from "./heroes/components/NavBar"
 import { AppRouter } from "./router/AppRouter"
 
 export const HeroesApp = () => {
     return (
         <>
             <AppRouter/>
+            
         </>
     )
 }
--- app6_heroesapp/src/router/AppRouter.jsx
+++ app6_heroesapp/src/router/AppRouter.jsx
@@ -2,12 +2,15 @@
 
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages/LoginPage"
+import { Navbar } from "../heroes/components/NavBar"
 import { DCPage } from "../heroes/pages/DCPage"
 import { MarvelPage } from "../heroes/pages/MarvelPage"
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar/> {/*Barra de navegación */}
+
         <Routes>
             <Route path="marvel" element={<MarvelPage />} />
             <Route path="dc" element={<DCPage />} />
