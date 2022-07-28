babff5300ecf3c019cca4ada752b34af704eb2ad
Marta Arenas <martilla311@gmail.com>
2022-07-28 13:03:11 +0200

    Creación de rutas privadas

--- app6_heroesapp/src/router/AppRouter.jsx
+++ app6_heroesapp/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages/LoginPage"
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
+import { PrivateRoutes } from "./PrivateRoutes"
 
 export const AppRouter = () => {
   return (
@@ -12,7 +13,13 @@ export const AppRouter = () => {
         <Routes>
 
             <Route path="login" element={<LoginPage />} />
-            <Route path="/*" element={<HeroesRoutes />} />
+
+            <Route path="/*" element={
+              <PrivateRoutes> {/* Si no está logeado, no deja entrar */}
+                <HeroesRoutes />
+              </PrivateRoutes>
+            } />
+             {/* <Route path="/*" element={<HeroesRoutes />} /> */}
 
         </Routes>
     </>
---
+++ app6_heroesapp/src/router/PrivateRoutes.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth/context/AuthContext";
+import { Navigate} from "react-router-dom"
+
+export const PrivateRoutes = ( {children} ) => {
+
+    const {state} = useContext(AuthContext);
+    const logged = state.user.logged;
+
+    return (logged) ? children :  <Navigate to="/login"/> // Si el usuario no está logeado, navega al login, y si si, va a las rutas del "children" que son el resto
+}
