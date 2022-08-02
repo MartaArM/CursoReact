99067b636537d86dad7e8115077ab4d5f636c413
Marta Arenas <martilla311@gmail.com>
2022-08-02 09:38:41 +0200

    Layout Journal

---
+++ app7_journal/src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const JournalLayout = ({children}) => {
+  return (
+    <Box sx={{ display: 'flex'}}>
+
+
+        <Box component='main' sx={{ flexGrow: 1, p:3}}>
+            {children}
+        </Box>
+
+    </Box>
+  )
+}
--- app7_journal/src/journal/pages/JournalPage.jsx
+++ app7_journal/src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
 import Typography from '@mui/material/Typography';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <Typography variant='h1'>JournalPage</Typography> // variant te pone para poner el estilo del elemento
+    // <Typography </JournalLayout>variant='h1'>JournalPage</Typography> // variant te pone para poner el estilo del elemento
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus ut vero itaque hic laudantium perferendis, nemo nobis recusandae soluta iure dolor? Perspiciatis quasi nostrum culpa inventore deserunt. Provident, soluta!
+      </Typography>
+    </JournalLayout>
   )
 }
