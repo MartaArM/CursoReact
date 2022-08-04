6049594a1ea381ea22296459c302bb091a59fa86
Marta Arenas <martilla311@gmail.com>
2022-08-04 10:28:09 +0200

    Pequeño cambio en pokemonapp

--- app8_toolkit/src/PokemonApp.jsx
+++ app8_toolkit/src/PokemonApp.jsx
@@ -4,26 +4,28 @@ import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
 
+    //Se llama para lanzar las acciones
     const dispatch = useDispatch();
     useEffect(() => { // Se llama cuando queremos ejecutar algo solo la primera vez
         dispatch(getPokemons());
     }, [])
 
+    // Cogemos el estado del store
     const {page, pokemons, isLoading} = useSelector((state) => state.pokemons);
 
-  return (
-    <>
-        <h1>Pokemon App</h1>
-        <hr/>
-        <span>Loading: { isLoading ? 'Cargando' : 'Cargado'}</span>
-        <ul>
-            {
-                pokemons.map( (pok) => (
-                    <li key={ pok.name }>{ pok.name }</li>
-                  ))
-            }
-        </ul>
-        <button onClick={() => dispatch(getPokemons(page))}>Siguiente</button>
-    </>
-  )
+    return (
+      <>
+          <h1>Pokemon App</h1>
+          <hr/>
+          <span>Loading: { isLoading ? 'Cargando' : 'Cargado'}</span>
+          <ul>
+              {
+                  pokemons.map( (pok) => (
+                      <li key={ pok.name }>{ pok.name }</li>
+                    ))
+              }
+          </ul>
+          <button onClick={() => dispatch(getPokemons(page))}>Siguiente</button>
+      </>
+    )
 }
