0b844468905333846bb85f6a98b953a478b9cd07
Marta Arenas <martilla311@gmail.com>
2022-08-10 12:51:18 +0200

    Custom hook

---
+++ app7_journal/src/hooks/useStatus.js
@@ -0,0 +1,26 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { firebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth/authSlice";
+
+export const useStatus = () => {
+  const {status} = useSelector( state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, async(user) => {
+      const {uid, email, displayName, photoURL} = user;
+      if (user) {
+        return dispatch(login({uid, email, displayName, photoURL}));
+      }
+      else {
+        return dispatch(logout());
+      }
+    }); // Se lanza cuando el status del usuario cambia
+  }, [])
+
+  return {
+    status
+  }
+}
