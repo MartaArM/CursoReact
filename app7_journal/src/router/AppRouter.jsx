71fcdf550f3603a0e59a946ac24d56e801632470
Marta Arenas <martilla311@gmail.com>
2022-08-10 12:50:50 +0200

    Arreglando AppRouter.jsx

--- app7_journal/src/router/AppRouter.jsx
+++ app7_journal/src/router/AppRouter.jsx
@@ -1,32 +1,12 @@
 
-import { onAuthStateChanged } from "firebase/auth"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
-import { firebaseAuth } from "../firebase/config"
 import { useStatus } from "../hooks/useStatus"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { login, logout } from "../store/auth/authSlice"
 import { CheckingAuth } from "../ui/components/CheckingAuth"
 
 export const AppRouter = () => {
-
-  // const {status} = useSelector( state => state.auth);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   onAuthStateChanged(firebaseAuth, async(user) => {
-  //     const {uid, email, displayName, photoURL} = user;
-  //     if (user) {
-  //       return dispatch(login({uid, email, displayName, photoURL}));
-  //     }
-  //     else {
-  //       return dispatch(logout());
-  //     }
-  //   }); // Se lanza cuando el status del usuario cambia
-  // }, [])
-  
+    
     const {status} = useStatus();
 
     if (status == 'checking') {
@@ -42,10 +22,6 @@ export const AppRouter = () => {
           }
 
           <Route path='/*' element = {<Navigate to='/auth/login' />} />
-
-            
-
-            
 
         </Routes>
       )  
