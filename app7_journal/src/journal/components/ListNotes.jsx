e73a31de1c4b339e45730110de701aefb1d2d63f
Marta Arenas <martilla311@gmail.com>
2022-08-11 17:35:43 +0200

    Seleccionando nota activa

--- app7_journal/src/journal/components/ListNotes.jsx
+++ app7_journal/src/journal/components/ListNotes.jsx
@@ -1,11 +1,21 @@
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { TurnedInNot } from '@mui/icons-material'
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+import { setActiveNote } from '../../store/journal/journalSlice'
 
 export const ListNotes = ({nota}) => {
+
+    const dispatch = useDispatch();
+
+    const onSelectNote = () => {
+        dispatch(setActiveNote(nota));
+    }
+    
+
   return (
     <ListItem key={ nota.id } disablePadding>
-        <ListItemButton>
+        <ListItemButton onClick={ onSelectNote}  >
             {/* Icono de la lista */}
             <ListItemIcon>
                 <TurnedInNot />
