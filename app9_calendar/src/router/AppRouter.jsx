862b785311860ea47f70afdb3fbb5df274823a44
Marta Arenas <martilla311@gmail.com>
2022-08-24 10:17:11 +0200

    Implementando rutas

---
+++ app9_calendar/src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth/pages/LoginPage'
+import { CalendarPage } from '../calendar/pages/CalendarPage'
+
+export const AppRouter = () => {
+
+    const status = 'non-authenticated'; //useStatus();
+
+    // if (status == 'checking') {
+    //   return <CheckingAuth /> // Si estamos comprobando el login, se carga una pantalla de espera con un circulo cargando
+    // }
+    // else {
+    return (
+    <Routes>
+
+        { ( status == 'authenticated')
+             ? <Route path="/*" element={<CalendarPage /* Diario / journal */ />} /> 
+             : <Route path="auth/*" element={<LoginPage /* Login y registro */ />}/> 
+        }
+
+        <Route path='/*' element = {<Navigate to='/auth/login' />} />
+
+    </Routes>
+    )  
+    // }
+  
+}
