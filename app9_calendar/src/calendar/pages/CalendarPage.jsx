b72d02863f47c2164184b989a32dc0b8bd8a3376
Marta Arenas <martilla311@gmail.com>
2022-08-24 13:19:56 +0200

    Componente para evento

--- app9_calendar/src/calendar/pages/CalendarPage.jsx
+++ app9_calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,15 +1,20 @@
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { NavBar } from "../components/NavBar"
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import {addHours} from 'date-fns'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { localizer } from "../../helpers/calendarLocalizer"
 import { getMessages } from "../../helpers/getMessages"
+import { EventBox } from "../components/EventBox"
 
 const events = [{
   title: 'Cumpleaños',
   notes: 'Hay que comprar tarta',
   start: new Date(),
-  end: addHours(new Date(), 2)
+  end: addHours(new Date(), 2),
+  user: {
+    _id: '123',
+    name: 'Marta'
+  }
 }]
 
 export const CalendarPage = () => {
@@ -38,6 +43,9 @@ export const CalendarPage = () => {
           style={{ height: 'calc( 100vh - 80px )' }} // El 100% de la vista - 80 pixeles de la barra de navegación
           messages={getMessages()}
           eventPropGetter={eventStyleGetter}
+          components={{ // Ponemos el componente nuevo que hemos creado para darle estilo
+            event: EventBox
+          }}
         />
 
       </>
