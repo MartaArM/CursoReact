792d5747dc354b06f197a918b7033fa03e6f4c5c
Marta Arenas <martilla311@gmail.com>
2022-08-31 12:02:48 +0200

    Creando esquema de usuario

--- app10_calendar_backend/controllers/auth.js
+++ app10_calendar_backend/controllers/auth.js
@@ -1,29 +1,35 @@
 // Funciones que se definen en las rutas
 const {validationResult} = require('express-validator');
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res) => {
+const crearUsuario = async(req, res) => {
 
     // Datos que mandamos en el body
-    const {name, email, password} = req.body;
+    const {email, password} = req.body;
 
-    // Manejo de errores
+    try {
+        let usuario = await Usuario.findOne({email: email});
+        console.log(usuario);
+
+        // const usuario = new Usuario(req.body); // Creamos un usuario
+        // await usuario.save();// Lo guardamos en la BD
+
+        return res.status(201).json({
+            ok: true,
+            msg: 'Crear usuario',
+            // name: name,
+            // email: email,
+            // password: password
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error. Hable con el administrador.'
+        })
+    }
+
     
-
-    // if (name.length < 3) {
-    //     return res.status(400).json({ //Mandamos el status 400 (bad request)
-    //         ok:false,
-    //         msg: "El nombre debe tener más de dos letras."
-    //     });
-    // }
-
-
-    return res.status(201).json({
-        ok: true,
-        msg: 'Crear usuario',
-        name: name,
-        email: email,
-        password: password
-    })
 }
 
 const iniciarSesion = (req, res) => {
