506272225091f4f73bbc3c7ff46c7faa51689a1a
Marta Arenas <martilla311@gmail.com>
2022-09-01 17:38:57 +0200

    Comprobación de campos

--- app10_calendar_backend/routes/events.js
+++ app10_calendar_backend/routes/events.js
@@ -1,5 +1,8 @@
 const {Router} = require('express');
+const { check } = require('express-validator');
 const { obtenerEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { esFecha } = require('../helpers/esFecha');
+const { validarCampos } = require('../middlewares/validacionCampos');
 const { validarJWT } = require('../middlewares/validacionJWT');
 
 const router = Router();
@@ -9,17 +12,20 @@ router.use(validarJWT); // Como lo tienen que usar todas, se puede poner aqui
 //Obtener eventos
 router.get(
     '/', 
-   // [
-     //   validarJWT
-   // ],
+    [
+      
+    ],
     obtenerEventos);
 
     //Crear un nuevo evento
 router.post(
     '/', 
-   // [
-     //   validarJWT
-   // ],
+   [
+    check('title', 'El título no puede ser vacío').not().isEmpty(),
+    check('start', 'La fecha de inicio no es correcta').custom(esFecha), // Se puede hacer un check personalizado
+    check('end', 'La fecha de fin no es correcta').custom(esFecha),
+    validarCampos
+   ],
     crearEvento);
 
 router.put(
