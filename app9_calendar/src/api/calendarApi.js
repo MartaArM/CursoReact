1da642e5c460537cb84b928aa3eb24e7ff248a2a
Marta Arenas <martilla311@gmail.com>
2022-09-07 13:57:55 +0200

    Mandamos el token en los headers

--- app9_calendar/src/api/calendarApi.js
+++ app9_calendar/src/api/calendarApi.js
@@ -5,6 +5,18 @@ const {VITE_API_URL} = getEnvVariables();
 
 const calendarApi = axios.create({
     baseURL: VITE_API_URL
+});
+
+// Interceptor que se ejecuta cuando se hace una petición
+// Lo utilizamos para meter el token en los headers de la petición
+calendarApi.interceptors.request.use (config => {
+
+    config.headers = {
+        ...config.headers, // Headers que podían estar ahí 
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config;
 })
 
 export default calendarApi;
