6084c8ddde619c98383c479157c67f396c6be1f6
Marta Arenas <martilla311@gmail.com>
2022-09-07 10:36:19 +0200

    Creando slice de autenticación

---
+++ app9_calendar/src/store/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    status: 'checking', // authenticated, not-authenticated
+    user: {},
+    errorMessage: undefined
+  },
+  reducers: {
+    setChecking: (state) => {
+        state.status = 'checking';
+        state.user = {};
+        state.errorMessage = undefined;
+    },
+    onLogin: (state, {payload}) => {
+        state.status = 'authenticated';
+        state.user = payload;
+        state.errorMessage = undefined;
+    }
+  }
+})
+export const { setChecking, onLogin} = authSlice.actions; 
